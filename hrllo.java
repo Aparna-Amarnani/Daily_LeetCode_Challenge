d33edc46f522e90f2dd3f77c5796d61702670921
Aparna Amarnani <71811435+Aparna-Amarnani@users.noreply.github.com>
2023-03-16 23:59:14 +0530

    Create hrllo.java

---
+++ hrllo.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int postEnd;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder.length != postorder.length || inorder.length == 0)
+            return null;
+        postEnd = postorder.length - 1;
+        return helper(inorder, postorder, 0, postEnd);
+    }
+
+    TreeNode helper(int[] inorder, int[] postorder, int start, int end){
+        if(start > end)
+            return null;
+        TreeNode curr = new TreeNode(postorder[postEnd--]);
+        if(start == end)
+            return curr;
+
+        int index = 0;
+        for(int i=end; i>=0; i--){
+            if(inorder[i] == curr.val){
+                index = i;
+                break;
+            }
+        }
+        curr.right = helper(inorder, postorder, index+1, end);
+        curr.left = helper(inorder, postorder, start, index-1);
+
+        return curr;
+    }
+}
